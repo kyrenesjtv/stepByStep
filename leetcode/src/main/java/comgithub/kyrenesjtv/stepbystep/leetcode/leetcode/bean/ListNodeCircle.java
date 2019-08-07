2af633adc2421ac7d4e718d6dad56191dcf485d0
albert0715 <sjtv5.12@gmail.com>
2019-08-07 10:25:30 +0800

    链表bean

---
+++ leetcode/src/main/java/comgithub/kyrenesjtv/stepbystep/leetcode/leetcode/bean/ListNodeCircle.java
@@ -0,0 +1,18 @@
+package comgithub.kyrenesjtv.stepbystep.leetcode.leetcode.bean;
+
+/**
+ * @ProjectName: stepByStep
+ * @Author: AlbertW
+ * @CreateDate: 2019/8/5 15:30
+ */
+public class ListNodeCircle {
+
+    public int val;
+    public ListNodeCircle next;
+
+    public ListNodeCircle(int x) {
+        val = x;
+        next=null;
+    }
+
+}
