88e1bb612adeebda861920ae3b5ef4c67f440f5d
huojianxiong <sjtv5.12@gmail.com>
2021-08-23 10:40:09 +0800

    优化龟兔赛跑

--- multithread/src/main/java/com/github/kyrenesjtv/multithread/video/Race.java
+++ multithread/src/main/java/com/github/kyrenesjtv/multithread/video/Race.java
@@ -5,41 +5,42 @@ package com.github.kyrenesjtv.multithread.video;
  * @Author: AlbertW
  * @CreateDate: 2021/8/22 22:54
  */
-public class Race implements Runnable{
+public class Race implements Runnable {
 
-    private volatile Boolean flag = false;
-    private String winner ;
+    private static volatile Boolean flag = false;
+    private String winner;
 
     @Override
     public void run() {
-        for (int i = 0; i < 100 ; i++) {
+        for (int i = 0; i < 100; i++) {
             String name = Thread.currentThread().getName();
-            Boolean aBoolean = gameStep(name, i);
-            if(aBoolean){
+            gameStep(name, i);
+            if (flag) {
                 break;
             }
-            System.out.println(name+ "跑了"+i+"步");
+            System.out.println(name + "跑了" + i + "步");
         }
     }
-    private Boolean gameStep(String name ,int i ){
-        if(name.equals("兔子")){
+
+    private void gameStep(String name, int i) {
+        if (name.equals("兔子")) {
             i = i * 2;
         }
-        if(name.equals("乌龟")){
-            i = i+1;
+        if (name.equals("乌龟")) {
+            i = i + 1;
         }
-        if(i == 100){
-            return true;
+        if (i == 100) {
+            System.out.println("游戏结束，胜利者是：" + name);
+            flag = true;
         }
-        return false;
     }
 
     public static void main(String[] args) {
         Race race1 = new Race();
         Race race2 = new Race();
 
-        new Thread(race1,"兔子").start();
-        new Thread(race2,"乌龟").start();
+        new Thread(race1, "兔子").start();
+        new Thread(race2, "乌龟").start();
 
     }
 
