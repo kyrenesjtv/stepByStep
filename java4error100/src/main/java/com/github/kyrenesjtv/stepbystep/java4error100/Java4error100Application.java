519faf62b321a25e536285cc30904475a1cfc96b
huojianxiong <sjtv5.12@gmail.com>
2021-12-28 22:28:11 +0800

    java开发错误100例-demo项目

---
+++ java4error100/src/main/java/com/github/kyrenesjtv/stepbystep/java4error100/Java4error100Application.java
@@ -0,0 +1,13 @@
+package com.github.kyrenesjtv.stepbystep.java4error100;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java4error100Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java4error100Application.class, args);
+    }
+
+}
---
+++ java4error100/src/test/java/com/github/kyrenesjtv/stepbystep/java4error100/Java4error100ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kyrenesjtv.stepbystep.java4error100;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java4error100ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
