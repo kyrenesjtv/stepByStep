b7ce889a44e7fa2e428164d806b32c4e966b3a69
huojianxiong <sjtv5.12@gmail.com>
2022-02-20 20:40:45 +0800

    设计模式-依赖反转原则

---
+++ design-model/src/main/java/com/github/kyrenesjtv/stepbystep/designmodel/designprinciple/dip/DIP.java
@@ -0,0 +1,17 @@
+package com.github.kyrenesjtv.stepbystep.designmodel.designprinciple.dip;
+
+/**
+ * @author huojianxiong
+ * @Description DIP
+ * @Date 2022/2/20 20:34
+ */
+public class DIP {
+
+    /*
+    依赖反转原则
+    高层模块（high-level modules）不要依赖低层模块（low-level）。
+    高层模块和低层模块应该通过抽象（abstractions）来互相依赖。
+    除此之外，抽象（abstractions）不要依赖具体实现细节（details），具体实现细节（details）依赖抽象（abstractions）。
+
+     */
+}
