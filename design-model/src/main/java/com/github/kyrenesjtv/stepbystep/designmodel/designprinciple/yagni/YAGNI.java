756ea0df2cdf2f28724a73f5e9a56cf57ead977f
huojianxiong <sjtv5.12@gmail.com>
2022-02-27 00:16:17 +0800

    设计模式-YAGNI原则，针对代码留好扩展点，不要将目前不需要的代码或配置或依赖过多的设计

---
+++ design-model/src/main/java/com/github/kyrenesjtv/stepbystep/designmodel/designprinciple/yagni/YAGNI.java
@@ -0,0 +1,16 @@
+package com.github.kyrenesjtv.stepbystep.designmodel.designprinciple.yagni;
+
+/**
+ * @author huojianxiong
+ * @Description YAGNI-You Ain’t Gonna Need It.
+ * @Date 2022/2/27 00:13
+ */
+public class YAGNI {
+
+    /*
+    你不会需要它。
+    这条原则也算是万金油了。当用在软件开发中的时候，它的意思是：不要去设计当前用不到的功能；不要去编写当前用不到的代码。
+    实际上，这条原则的核心思想就是：不要做过度设计。
+    但是遇到日后可能会增加的不同类型的代码，需要留好配置点（开闭）
+     */
+}
