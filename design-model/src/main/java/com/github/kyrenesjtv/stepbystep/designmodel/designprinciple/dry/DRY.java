78855689cee08dc7c1f23830dfa083ceef3434a0
huojianxiong <sjtv5.12@gmail.com>
2022-02-27 21:39:34 +0800

    设计模式-DRY原则 - Don’t Repeat Yourself。 不要重复造轮子

---
+++ design-model/src/main/java/com/github/kyrenesjtv/stepbystep/designmodel/designprinciple/dry/DRY.java
@@ -0,0 +1,19 @@
+package com.github.kyrenesjtv.stepbystep.designmodel.designprinciple.dry;
+
+/**
+ * @author huojianxiong
+ * @Description DRY - Don’t Repeat Yourself。 不要重复造轮子
+ * @Date 2022/2/27 21:34
+ */
+public class DRY {
+
+    /*
+    代码的重复有3种情况
+    1：实现逻辑重复
+        功能不同，实现逻辑相同。不重复
+    2：功能语义重复
+        函数命名不同，实现逻辑不同，但是功能是相同的。重复
+    3：代码执行重复
+        代码在顺序执行的时候，同一方法被通次执行。重复
+     */
+}
