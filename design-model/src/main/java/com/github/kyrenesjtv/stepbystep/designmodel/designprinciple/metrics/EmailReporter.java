dfea0a7647c56e7c506db5927c11d68bd4c4c7eb
huojianxiong <sjtv5.12@gmail.com>
2022-03-03 13:35:23 +0800

    设计模式-统计接口性能（补充展示）

---
+++ design-model/src/main/java/com/github/kyrenesjtv/stepbystep/designmodel/designprinciple/metrics/EmailReporter.java
@@ -0,0 +1,62 @@
+package com.github.kyrenesjtv.stepbystep.designmodel.designprinciple.metrics;
+
+import java.util.*;
+
+/**
+ * @author huojianxiong
+ * @Description EmailReporter - email发送统计信息
+ * @Date 2022/3/3 13:21
+ */
+public class EmailReporter {
+    private static final Long DAY_HOURS_IN_SECONDS = 86400L;
+
+    private MetricsStorage metricsStorage;
+    private EmailSender emailSender;
+    private List<String> toAddresses = new ArrayList<>();
+
+    public EmailReporter(MetricsStorage metricsStorage) {
+        this(metricsStorage, new EmailSender(/*省略参数*/));
+    }
+
+    public EmailReporter(MetricsStorage metricsStorage, EmailSender emailSender) {
+        this.metricsStorage = metricsStorage;
+        this.emailSender = emailSender;
+    }
+
+    public void addToAddress(String address) {
+        toAddresses.add(address);
+    }
+
+    public void startDailyReport() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DATE, 1);
+        calendar.add(Calendar.HOUR_OF_DAY, 0);
+        calendar.add(Calendar.MINUTE, 0);
+        calendar.add(Calendar.SECOND, 0);
+        calendar.add(Calendar.MILLISECOND, 0);
+        Date firstTime = calendar.getTime();
+        Timer timer = new Timer();
+
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                long durationInMillis = DAY_HOURS_IN_SECONDS * 1000;
+                long endTimeInMillis = System.currentTimeMillis();
+                long startTimeInMillis = endTimeInMillis - durationInMillis;
+                Map<String, List<RequestInfo>> requestInfos = metricsStorage.getRequestInfos(startTimeInMillis, endTimeInMillis);
+                Map<String, RequestStat> stats = new HashMap<>();
+                requestInfos.forEach((k, v) -> {
+                    RequestStat aggregate = Aggregator.aggregate(v, durationInMillis);
+                    stats.put(k, aggregate);
+                });
+                //todo 格式化成html 循环toAddresses发送邮箱
+                for (String toAddress : toAddresses) {
+
+                }
+            }
+        }, firstTime, DAY_HOURS_IN_SECONDS * 1000);
+
+    }
+
+
+}
---
+++ design-model/src/main/java/com/github/kyrenesjtv/stepbystep/designmodel/designprinciple/metrics/EmailSender.java
@@ -0,0 +1,9 @@
+package com.github.kyrenesjtv.stepbystep.designmodel.designprinciple.metrics;
+
+/**
+ * @author huojianxiong
+ * @Description EmailSender
+ * @Date 2022/3/3 13:30
+ */
+public class EmailSender {
+}
