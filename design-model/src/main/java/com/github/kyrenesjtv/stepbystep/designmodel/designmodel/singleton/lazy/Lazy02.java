c43451266f2d702af557c695da0e0d15d1e9a7a3
huojianxiong <sjtv5.12@gmail.com>
2022-03-05 01:14:33 +0800

    设计模式-单例模式-懒汉式02，加上了synchronized

---
+++ design-model/src/main/java/com/github/kyrenesjtv/stepbystep/designmodel/designmodel/singleton/lazy/Lazy02.java
@@ -0,0 +1,18 @@
+package com.github.kyrenesjtv.stepbystep.designmodel.designmodel.singleton.lazy;
+
+/**
+ * @author huojianxiong
+ * @Description Lazy01 - 懒汉式
+ * @Date 2022/3/5 01:11
+ */
+public class Lazy02 {
+
+    private static Lazy02 lazy01 = null;
+
+    public static synchronized Lazy02 getInstance() {
+        if (lazy01 == null) {
+            lazy01 = new Lazy02();
+        }
+        return lazy01;
+    }
+}
