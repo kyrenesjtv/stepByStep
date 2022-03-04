688928a6840ebe62772ce38ca5dff93b116c4591
huojianxiong <sjtv5.12@gmail.com>
2022-03-05 01:14:01 +0800

    设计模式-单例模式-懒汉式01

---
+++ design-model/src/main/java/com/github/kyrenesjtv/stepbystep/designmodel/designmodel/singleton/lazy/Lazy01.java
@@ -0,0 +1,18 @@
+package com.github.kyrenesjtv.stepbystep.designmodel.designmodel.singleton.lazy;
+
+/**
+ * @author huojianxiong
+ * @Description Lazy01 - 懒汉式
+ * @Date 2022/3/5 01:11
+ */
+public class Lazy01 {
+
+    private static Lazy01 lazy01 = null;
+
+    public static Lazy01 getInstance() {
+        if (lazy01 == null) {
+            lazy01 = new Lazy01();
+        }
+        return lazy01;
+    }
+}
