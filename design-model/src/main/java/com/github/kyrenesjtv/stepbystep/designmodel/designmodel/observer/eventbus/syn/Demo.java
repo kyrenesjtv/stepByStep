70ab0700f32e9f978b864586e128bbeb18f0534a
huojianxiong <sjtv5.12@gmail.com>
2022-03-24 11:25:39 +0800

    设计模式-观察者模式-用eventbus，如果被观察者的入参不同的话，只会针对性的接收

--- design-model/src/main/java/com/github/kyrenesjtv/stepbystep/designmodel/designmodel/observer/eventbus/syn/Demo.java
+++ design-model/src/main/java/com/github/kyrenesjtv/stepbystep/designmodel/designmodel/observer/eventbus/syn/Demo.java
@@ -2,7 +2,6 @@ package com.github.kyrenesjtv.stepbystep.designmodel.designmodel.observer.eventb
 
 import com.github.kyrenesjtv.stepbystep.designmodel.designmodel.observer.eventbus.syn.entity.EventBO1;
 import com.github.kyrenesjtv.stepbystep.designmodel.designmodel.observer.eventbus.syn.listener.ListenerA;
-import com.github.kyrenesjtv.stepbystep.designmodel.designmodel.observer.eventbus.syn.listener.ListenerB;
 import com.google.common.eventbus.EventBus;
 
 /**
@@ -15,7 +14,7 @@ public class Demo {
     public static void main(String[] args) {
         EventBus eventBus = new EventBus("syn");
         eventBus.register(new ListenerA());
-        eventBus.register(new ListenerB());
+//        eventBus.register(new ListenerB());
         eventBus.post(new EventBO1(1, "123"));
     }
 }
--- design-model/src/main/java/com/github/kyrenesjtv/stepbystep/designmodel/designmodel/observer/eventbus/syn/listener/ListenerA.java
+++ design-model/src/main/java/com/github/kyrenesjtv/stepbystep/designmodel/designmodel/observer/eventbus/syn/listener/ListenerA.java
@@ -2,6 +2,7 @@ package com.github.kyrenesjtv.stepbystep.designmodel.designmodel.observer.eventb
 
 import com.alibaba.fastjson.JSON;
 import com.github.kyrenesjtv.stepbystep.designmodel.designmodel.observer.eventbus.syn.entity.EventBO1;
+import com.github.kyrenesjtv.stepbystep.designmodel.designmodel.observer.eventbus.syn.entity.EventBO2;
 import com.google.common.eventbus.Subscribe;
 
 /**
@@ -13,6 +14,11 @@ public class ListenerA {
 
     @Subscribe
     public void doSomethingA(EventBO1 eventBO1) {
-        System.out.println("doSomethingA" + JSON.toJSON(eventBO1));
+        System.out.println("doSomethingA-eventBO1" + JSON.toJSON(eventBO1));
+    }
+
+    @Subscribe
+    public void doSomethingA01(EventBO2 eventBO2) {
+        System.out.println("doSomethingA-eventBO2" + JSON.toJSON(eventBO2));
     }
 }
