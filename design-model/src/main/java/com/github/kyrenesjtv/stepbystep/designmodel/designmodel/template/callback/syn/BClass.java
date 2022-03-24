0895e73b85b2235aebba125b6f98c25418efda2e
huojianxiong <sjtv5.12@gmail.com>
2022-03-24 17:04:44 +0800

    设计模式-模板模式-回调，同步的方式

---
+++ design-model/src/main/java/com/github/kyrenesjtv/stepbystep/designmodel/designmodel/template/callback/syn/BClass.java
@@ -0,0 +1,16 @@
+package com.github.kyrenesjtv.stepbystep.designmodel.designmodel.template.callback.syn;
+
+/**
+ * @author huojianxiong
+ * @Description BClass
+ * @Date 2022/3/24 16:32
+ */
+public class BClass {
+
+    public void process(ICallback callback) {
+        System.out.println("before");
+        callback.methodToCallback();
+        System.out.println("after");
+    }
+
+}
---
+++ design-model/src/main/java/com/github/kyrenesjtv/stepbystep/designmodel/designmodel/template/callback/syn/ICallback.java
@@ -0,0 +1,10 @@
+package com.github.kyrenesjtv.stepbystep.designmodel.designmodel.template.callback.syn;
+
+/**
+ * @author huojianxiong
+ * @Description ICallback
+ * @Date 2022/3/24 16:31
+ */
+public interface ICallback {
+    void methodToCallback();
+}
